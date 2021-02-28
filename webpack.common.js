c1556cfcb283e2b3ea933804996802d79dcfcc5a
Przemysław Wysocki <przewysocki@gmail.com>
2021-02-28 13:43:01 +0100

    initial

--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/testing.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "testing.html",
+    }),
   ],
 };
